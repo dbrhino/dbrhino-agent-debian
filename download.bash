c4956fd1174459374369543b9b244835fd683089
Buck Ryan <buck.ryan2@gmail.com>
2018-02-16 14:58:56 -0500

    prepare 0.4.5

--- download.bash
+++ download.bash
@@ -2,7 +2,7 @@
 version=$(cat VERSION)
 if [[ ! -f dbrhino-agent_${version}.orig.tar.gz ]]; then
     wget -O dbrhino-agent_${version}.orig.tar.gz \
-        https://github.com/dbrhino/dbrhino-agent/archive/${version}.tar.gz
+        https://github.com/dbrhino/dbrhino-agent/archive/v${version}.tar.gz
 fi
 tar xzvf dbrhino-agent_${version}.orig.tar.gz \
     -C dbrhino-agent \
